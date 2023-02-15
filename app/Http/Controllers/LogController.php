e3fba64cf423b1b4b79be389eac6d2914f18590d
Nahin <jalal@orangebd.com>
2023-02-15 11:38:02 +0600

    log module added

---
+++ app/Http/Controllers/LogController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Log;
+use Illuminate\Http\Request;
+
+class LogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $file = file(storage_path() . '/' . 'app' . '/public/' . '/api/' . 'log2.log');
+//        dd($file);
+
+        $datas = json_decode(json_encode($file), true);
+//        dd($datas);
+        $lines = array();
+        foreach($datas as  $value) {
+            $parts = explode(' ', $value);
+            $lines[] = array(
+
+                'date' => ltrim($parts[0], '[') . ' ' . rtrim($parts[1], ']'),
+                'port' => $parts[2],
+                'ip1' => $parts[3],
+                'ip2' => $parts[4]
+            );
+            echo "<pre>";
+
+
+        }
+        print_r($lines);
+
+
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Log  $log
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Log $log)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Log  $log
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Log $log)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Log  $log
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Log $log)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Log  $log
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Log $log)
+    {
+        //
+    }
+}
