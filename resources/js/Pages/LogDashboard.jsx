c5b1916ce1c246b9c2b4c7d5d4a7b5ae73553100
Nahin <jalal@orangebd.com>
2023-02-15 17:13:57 +0600

    bar chart modified

--- resources/js/Pages/LogDashboard.jsx
+++ resources/js/Pages/LogDashboard.jsx
@@ -71,8 +71,20 @@ export default function Dashboard(props) {
             datasets: [
                 {
                     label: 'Users Gained',
-                    backgroundColor: 'rgba(20,54,166,0.93)',
-                    borderColor: 'rgba(20,54,166,0.6)',
+                    backgroundColor: [
+                        '#B21F00',
+                        '#C9DE00',
+                        '#2FDE00',
+                        '#00A6B4',
+                        '#6800B4'
+                    ],
+                    hoverBackgroundColor: [
+                        '#501800',
+                        '#4B5000',
+                        '#175000',
+                        '#003350',
+                        '#35014F'
+                    ],
                     borderWidth: 1,
                     data: [65, 59, 80, 81, 56, 55, 40],
                 }
