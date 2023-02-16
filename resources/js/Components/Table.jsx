b0406896518d5d00ddf4090a70c2cbe96dd68ecf
Nahin <jalal@orangebd.com>
2023-02-16 11:52:58 +0600

    table design updated

--- resources/js/Components/Table.jsx
+++ resources/js/Components/Table.jsx
@@ -4,61 +4,61 @@ import React from "react";
 
 function Table() {
     return (
-        <div>
+        <div className="overflow-x-scroll	">
 
-            <table className="table-auto w-full text-center mt-6">
+            <table className="table-auto w-full text-center mt-6 border-collapse">
                 <thead>
-                <tr>
-                    <th>Plan Name</th>
-                    <th>Customer</th>
-                    <th>/</th>
-                    <th>/login</th>
-                    <th>/orders</th>
-                    <th>/profile</th>
-                    <th>/shoppingcart</th>
-                    <th>/total</th>
+                <tr className="divide-x divide-gray-400 border border-gray-400">
+                    <th className="px-2">Plan Name</th>
+                    <th className="px-2">Customer</th>
+                    <th className="px-2">/</th>
+                    <th className="px-2">/login</th>
+                    <th className="px-2">/orders</th>
+                    <th className="px-2">/profile</th>
+                    <th className="px-2">/shoppingcart</th>
+                    <th className="px-2">/total</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>a</td>
-                    <td>a</td>
+                <tr className="divide-x divide-gray-400 border border-gray-400">
+                    <td className="px-2">a</td>
+                    <td className="px-2">a</td>
                     <td className="bg-red-500">a</td>
                     <td className="bg-orange-400">a</td>
                     <td className="bg-orange-400">a</td>
                     <td className="bg-orange-400">a</td>
                     <td className="bg-orange-600">a</td>
-                    <td>a</td>
+                    <td className="px-2">a</td>
                 </tr>
-                <tr>
-                    <td>a</td>
-                    <td>a</td>
+                <tr className="divide-x divide-gray-400 border border-gray-400">
+                    <td className="px-2">a</td>
+                    <td className="px-2">a</td>
                     <td className="bg-red-500">a</td>
                     <td className="bg-orange-400">a</td>
                     <td className="bg-orange-400">a</td>
                     <td className="bg-orange-400">a</td>
                     <td className="bg-orange-600">a</td>
-                    <td>a</td>
+                    <td className="px-2">a</td>
                 </tr>
-                <tr>
-                    <td>a</td>
-                    <td>a</td>
+                <tr className="divide-x divide-gray-400 border border-gray-400">
+                    <td className="px-2">a</td>
+                    <td className="px-2">a</td>
                     <td className="bg-red-500">a</td>
                     <td className="bg-orange-400">a</td>
                     <td className="bg-orange-400">a</td>
                     <td className="bg-orange-400">a</td>
                     <td className="bg-orange-600">a</td>
-                    <td>a</td>
+                    <td className="px-2">a</td>
                 </tr>
-                <tr>
-                    <td>a</td>
-                    <td>a</td>
+                <tr className="divide-x divide-gray-400 border border-gray-400">
+                    <td className="px-2">a</td>
+                    <td className="px-2">a</td>
                     <td className="bg-red-500">a</td>
                     <td className="bg-orange-400">a</td>
                     <td className="bg-orange-400">a</td>
                     <td className="bg-orange-400">a</td>
                     <td className="bg-orange-600">a</td>
-                    <td>a</td>
+                    <td className="px-2">a</td>
                 </tr>
                 </tbody>
             </table>
