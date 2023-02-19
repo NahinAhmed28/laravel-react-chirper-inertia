e16a8a9ea80f2e4de1ce30e9ac505bd8f36a03db
Nahin <jalal@orangebd.com>
2023-02-19 13:35:39 +0600

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'API-MANAGER') }}</title>
+
+
+        <link rel = "icon" href =
+            "https://media.geeksforgeeks.org/wp-content/cdn-uploads/gfg_200X200.png"
+              type = "image/x-icon">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
